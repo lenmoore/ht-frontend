4c4b3326c88392f3661715af0f8fd0e6ba38d800
helenavainmaa <helena.vainmaa@fooddocs.com>
2024-01-09 11:36:50 +0200

    Fix folders

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,14 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="logo" alt="Vite logo"/>
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="assets/vue.svg" class="logo vue" alt="Vue logo"/>
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  camera tutorial: <a href="https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Recording_a_media_element">here</a>
+  <HelloWorld msg="Vite + Vue"/>
 </template>
 
 <style scoped>
@@ -21,9 +22,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
