5f3790edd91ca94aa34314a0e06165aa1be1927a
helenavainmaa <helena.vainmaa@fooddocs.com>
2024-01-25 08:22:42 +0200

    local https setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        basicSsl(),
+    ],
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: `@import "src/_variables.scss";`
-            }
-        }
+                additionalData: `@import "src/_variables.scss";`,
+            },
+        },
     },
 
 });
