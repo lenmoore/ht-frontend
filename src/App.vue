254cfde49beec6f1d4b48a4ef65d2e2ccc0bf4c5
helenavainmaa <helena.vainmaa@fooddocs.com>
2024-02-14 21:25:43 +0200

    github ci/cd

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,37 @@
 <template>
-    <div>
-        <NavMenu/>
+  <div>
+    <NavMenu />
 
-        <!--        <div>-->
+    Git works :)
+    <!--        <div>-->
 
-        <!--            camera tutorial:-->
-        <!--            <a-->
-        <!--                href="https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Recording_a_media_element"-->
-        <!--            >here</a-->
-        <!--            >-->
-        <!--        </div>-->
+    <!--            camera tutorial:-->
+    <!--            <a-->
+    <!--                href="https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Recording_a_media_element"-->
+    <!--            >here</a-->
+    <!--            >-->
+    <!--        </div>-->
 
-        <RouterView class="container"/>
-        <footer>
-            :)
-        </footer>
-    </div>
+    <RouterView class="container" />
+    <footer>:)</footer>
+  </div>
 </template>
 <style lang="scss">
-
 .nav {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    width: 100%;
-    height: 50px;
-    font-size: 20px;
-    font-weight: bold;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 100%;
+  height: 50px;
+  font-size: 20px;
+  font-weight: bold;
+  align-items: center;
 
-    a {
-        padding: 1rem;
-    }
+  a {
+    padding: 1rem;
+  }
 }
 </style>
 <script setup lang="ts">
-import NavMenu from './components/NavMenu.vue';
+import NavMenu from "./components/NavMenu.vue";
 </script>
