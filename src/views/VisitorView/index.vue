67b35fe2483ad646f5c6f24e81aa159560396f6e
helenavainmaa <helena.vainmaa@fooddocs.com>
2024-03-24 15:12:11 +0200

    try to get higher quality for videos

--- src/views/VisitorView/index.vue
+++ src/views/VisitorView/index.vue
@@ -242,7 +242,20 @@ export default {
     async startRecording(stream) {
       const lengthInMS = this.currentTask.duration * 1000;
       console.log(lengthInMS);
-      let recorder = new MediaRecorder(stream);
+      let options = { mimeType: 'video/webm; codecs="av01.0.05M.08"' };
+      if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+        console.log(`${options.mimeType} is not Supported`);
+        options = { mimeType: 'video/webm; codecs="vp9"' }; // Fallback to VP9
+        if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+          console.log(`${options.mimeType} is not Supported`);
+          options = { mimeType: 'video/webm; codecs="vp8"' }; // Fallback to VP8
+          if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+            console.log(`${options.mimeType} is not Supported`);
+            options = { mimeType: "video/webm" }; // Fallback to default WebM if nothing else is supported
+          }
+        }
+      }
+      let recorder = new MediaRecorder(stream, options);
       let data = [];
 
       recorder.ondataavailable = (event) => data.push(event.data);
