d5d9cb072f04be7d6caf76f56d0480d663be42d3
helenavainmaa <helena.vainmaa@fooddocs.com>
2024-06-05 10:41:36 +0300

    remove alert

--- src/views/VisitorView/index.vue
+++ src/views/VisitorView/index.vue
@@ -118,7 +118,6 @@ export default {
         this.displayFileName,
       );
 
-      alert("Kinnitatud!");
       this.taskIsActive = false;
       this.currentTask = null;
       this.recorderOpen = false;
